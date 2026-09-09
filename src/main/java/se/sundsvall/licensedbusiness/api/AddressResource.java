8cab936bf09bff7c100b2232f0a0222340e68517
AndreSoderlund <andre.soderlund@b3.se>
2026-09-09 10:52:39 +0200

    Add OpenAPI documentation to Address, Assignment and RestaurantNumber resources

--- src/main/java/se/sundsvall/licensedbusiness/api/AddressResource.java
+++ src/main/java/se/sundsvall/licensedbusiness/api/AddressResource.java
@@ -1,5 +1,11 @@
 package se.sundsvall.licensedbusiness.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,29 +19,55 @@ import se.sundsvall.dept44.common.validators.annotation.ValidMunicipalityId;
 import se.sundsvall.dept44.problem.Problem;
 
 import static org.springframework.http.HttpStatus.NOT_IMPLEMENTED;
+import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE;
 
 @Validated
 @RestController
+@Tag(name = "Address Resources")
 @RequestMapping("/{municipalityId}/addresses")
+@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
+@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
+@ApiResponse(responseCode = "502", description = "Bad Gateway", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
 class AddressResource {
 
+	@Operation(summary = "Get a paged list of addresses", responses = {
+		@ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true)
+	})
 	@GetMapping
-	ResponseEntity<Void> getAddresses(@PathVariable @ValidMunicipalityId final String municipalityId, final Pageable pageable) {
+	ResponseEntity<Void> getAddresses(
+		@Parameter(name = "municipalityId", description = "Municipality ID", example = "2281") @PathVariable @ValidMunicipalityId final String municipalityId,
+		final Pageable pageable) {
 		throw Problem.valueOf(NOT_IMPLEMENTED, "Not yet implemented");
 	}
 
+	@Operation(summary = "Get an address by ID", responses = {
+		@ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true),
+		@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
+	})
 	@GetMapping("/{addressId}")
-	ResponseEntity<Void> getAddress(@PathVariable @ValidMunicipalityId final String municipalityId, @PathVariable final String addressId) {
+	ResponseEntity<Void> getAddress(
+		@Parameter(name = "municipalityId", description = "Municipality ID", example = "2281") @PathVariable @ValidMunicipalityId final String municipalityId,
+		@Parameter(name = "addressId", description = "Address ID", example = "9ce333ec-a473-438b-8406-a71e957dc107") @PathVariable final String addressId) {
 		throw Problem.valueOf(NOT_IMPLEMENTED, "Not yet implemented");
 	}
 
+	@Operation(summary = "Search addresses", responses = {
+		@ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true)
+	})
 	@GetMapping("/search")
-	ResponseEntity<Void> searchAddresses(@PathVariable @ValidMunicipalityId final String municipalityId, @RequestParam final String query, final Pageable pageable) {
+	ResponseEntity<Void> searchAddresses(
+		@Parameter(name = "municipalityId", description = "Municipality ID", example = "2281") @PathVariable @ValidMunicipalityId final String municipalityId,
+		@Parameter(name = "query", description = "Search query", example = "Storgatan 1") @RequestParam final String query,
+		final Pageable pageable) {
 		throw Problem.valueOf(NOT_IMPLEMENTED, "Not yet implemented");
 	}
 
+	@Operation(summary = "Create an address", responses = {
+		@ApiResponse(responseCode = "201", description = "Created")
+	})
 	@PostMapping
-	ResponseEntity<Void> createAddress(@PathVariable @ValidMunicipalityId final String municipalityId) {
+	ResponseEntity<Void> createAddress(
+		@Parameter(name = "municipalityId", description = "Municipality ID", example = "2281") @PathVariable @ValidMunicipalityId final String municipalityId) {
 		throw Problem.valueOf(NOT_IMPLEMENTED, "Not yet implemented");
 	}
 }
--- src/main/java/se/sundsvall/licensedbusiness/api/AssignmentResource.java
+++ src/main/java/se/sundsvall/licensedbusiness/api/AssignmentResource.java
@@ -1,5 +1,11 @@
 package se.sundsvall.licensedbusiness.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -11,19 +17,34 @@ import se.sundsvall.dept44.common.validators.annotation.ValidMunicipalityId;
 import se.sundsvall.dept44.problem.Problem;
 
 import static org.springframework.http.HttpStatus.NOT_IMPLEMENTED;
+import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE;
 
 @Validated
 @RestController
+@Tag(name = "Assignment Resources")
 @RequestMapping("/{municipalityId}/assignments")
+@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
+@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
+@ApiResponse(responseCode = "502", description = "Bad Gateway", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
 class AssignmentResource {
 
+	@Operation(summary = "Create an assignment", responses = {
+		@ApiResponse(responseCode = "201", description = "Created")
+	})
 	@PostMapping
-	ResponseEntity<Void> createAssignment(@PathVariable @ValidMunicipalityId final String municipalityId) {
+	ResponseEntity<Void> createAssignment(
+		@Parameter(name = "municipalityId", description = "Municipality ID", example = "2281") @PathVariable @ValidMunicipalityId final String municipalityId) {
 		throw Problem.valueOf(NOT_IMPLEMENTED, "Not yet implemented");
 	}
 
+	@Operation(summary = "Update an assignment", responses = {
+		@ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true),
+		@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
+	})
 	@PatchMapping("/{assignmentId}")
-	ResponseEntity<Void> updateAssignment(@PathVariable @ValidMunicipalityId final String municipalityId, @PathVariable final String assignmentId) {
+	ResponseEntity<Void> updateAssignment(
+		@Parameter(name = "municipalityId", description = "Municipality ID", example = "2281") @PathVariable @ValidMunicipalityId final String municipalityId,
+		@Parameter(name = "assignmentId", description = "Assignment ID", example = "9ce333ec-a473-438b-8406-a71e957dc107") @PathVariable final String assignmentId) {
 		throw Problem.valueOf(NOT_IMPLEMENTED, "Not yet implemented");
 	}
 }
